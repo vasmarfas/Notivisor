6d4380a319e41dfca7100d050a5b534e9e44211b
vasmarfas <vasmarfas@mail.ru>
2026-09-13 22:39:39 +0300

    Bump application version to 1.2.1.
    - Increment `versionCode` to 5.
    - Update `versionName` to 1.2.1.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.vasmarfas.notivisor"
         minSdk = 26
         targetSdk = 37
-        versionCode = 4
-        versionName = "1.2.0"
+        versionCode = 5
+        versionName = "1.2.1"
     }
 
     buildTypes {
